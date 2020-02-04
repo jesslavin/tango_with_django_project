d1e8d344e4252dfa7c5818b9c9e6e0a3118c1211
Jessica Lavin <2495543l@student.gla.ac.uk>
2020-02-04 13:27:25 +0000

    up to chapter 4

--- rango/urls.py
+++ rango/urls.py
@@ -1,8 +1,10 @@
+from django.conf.urls import url
 from django.urls import path
 from rango import views
 
 app_name = 'rango'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
