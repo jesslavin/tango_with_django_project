595a7fe15de8abd5dcf2b5671f3c4ccf8941bbfb
Jessica Lavin <2495543l@student.gla.ac.uk>
2020-02-11 16:20:46 +0000

    minor changes fixed

--- rango/views.py
+++ rango/views.py
@@ -81,5 +81,4 @@ def show_category(request, category_name_slug):
 
 def about(request):
     context_dict = {}
-    context_dict['boldmessage'] = 'This tutorial has been put together by Jessica Lavin'
     return render(request, 'rango/about.html', context=context_dict)
